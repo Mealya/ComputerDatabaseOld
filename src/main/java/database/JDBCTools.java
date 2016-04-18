ed2f85ed80ecbd994b5cbea7a16acbf246494e93
mangot <mangot@excilys.com>
2016-04-18 16:59:54 +0200

    Passage du fichier JDBCTools à gestion de DB multiples Ajout d'un fichier XML Maven -> mysql

--- src/main/java/database/JDBCTools.java
+++ src/main/java/database/JDBCTools.java
@@ -1,68 +1,31 @@
 package database;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Angot Maxime on 18/04/16.
  */
 public class JDBCTools {
 
-    private static Connection connectTODB = null;
 
+    private static Map<String, Connection> connections = new HashMap<String, Connection>();
+
+    /*
+        //public static Connection connectTODB = null;
     public static void main(String[] argv) throws SQLException {
         JDBCTools tool = new JDBCTools();
+        tool.connectToMySql("computer-database-db");
         tool.getComputers();
         tool.closeConnect();
         tool.getComputers();
-    }
-
-    public void connectToMySql() throws SQLException {
-        if (connectTODB != null)  {
-            if (!connectTODB.isClosed()) {
-                return;
-            }
-        }
-        System.out.println("-------- MySQL JDBC Connection Testing ------------");
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            //TODO gérer les throws
-            System.out.println("Where is your MySQL JDBC Driver?");
-            e.printStackTrace();
-            return;
-        }
-
-        System.out.println("MySQL JDBC Driver Registered!");
-        Connection connection = null;
-
-        try {
-            connection = DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/computer-database-db?zeroDateTimeBehavior=convertToNull",
-                            "admincdb", "qwerty1234");
-
-        } catch (SQLException e) {
-            //TODO gérer les throws
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-            connectTODB = connection;
-        } else {
-            //TODO gérer les throws
-            System.out.println("Failed to make connection!");
-            return;
-        }
-
+        System.out.println(JDBCTools.connectTODB.getCatalog());
     }
 
     public void getComputers() throws SQLException {
-
         if (connectTODB == null) {
-            connectToMySql();
+            return;
         }
         if (!connectTODB.isClosed()) {
 
@@ -91,10 +54,71 @@ public class JDBCTools {
             rs.close();
         }
     }
+    */
 
-    public void closeConnect() throws SQLException {
-        if (connectTODB != null) {
-            connectTODB.close();
+
+
+    public void connectToMySql(String name) throws SQLException {
+        if (name == null) {
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        if (connections.get(name) != null) {
+            return;
+        }
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            //TODO gérer les throws
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+ name +"?zeroDateTimeBehavior=convertToNull",
+                            "admincdb", "qwerty1234");
+
+        } catch (SQLException e) {
+            //TODO gérer les throws
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+            //connectTODB = connection;
+            connections.put(name, connection);
+        } else {
+            //TODO gérer les throws
+            System.out.println("Failed to make connection!");
+            return;
+        }
+
+    }
+
+    public Connection getConnection(String name) throws SQLException {
+        if (name == null) {
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        return connections.get(name);
+    }
+
+    public void closeConnect(String name) throws SQLException {
+        if (name == null) {
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        for (Map.Entry<String, Connection> c : connections.entrySet()) {
+            if (c.getKey().equals(name)) {
+                c.getValue().close();
+                connections.remove(c);
+            }
         }
     }
 }
